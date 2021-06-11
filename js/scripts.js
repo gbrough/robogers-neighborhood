53e92cb4225c887dd5fc5ae8e05dda6e5288eebb
gbrough <gbrough@gmail.com>
2021-06-11 10:59:12 -0700

    updated logic for returning array results

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-function getRoboMessage(numberinput) {
-  let roboMessage = ""
+const numberArray = [0,1,2,3,4,5,6,7,8,9];
 
+function getRoboMessage(numberinput) {
+  numberArray.forEach(function(element) {
   if (numberinput === 1) {
-    roboMessage = "Beep"
+    roboMessage = "Beep!"
   } else if (numberinput === 2) {
-    roboMessage = "Boop"
-  } else {
+    roboMessage = "Boop!"
+  } else if (numberinput === 3) {
     roboMessage = "Won't you be my neighbor?"
+  } else {
+    roboMessage = numberinput
   }
-  return roboMessage;
+  });
+  return roboMessage;  
 }
+getRoboMessage()
+
 
 $(document).ready(function () {
   $('form').submit(function (event) {
