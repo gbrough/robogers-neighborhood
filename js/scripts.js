af1fc987ba25db515be0bebfb04eb2b609ce3b04
gbrough <gbrough@gmail.com>
2021-06-11 13:57:01 -0700

    add logic for include method

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
-const numberArray = [0,1,2,3,4];
+const numberArray = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13"];
 let newArray = []
 
 function getRoboMessage(numberinput) {
   for (i = 0; i < numberArray.length; i++) {
-    if (i === 1) {
-    newArray.push(" beep!")
-  } else if (i === 2) {
-    newArray.push(" boop!")
+    if (numberArray[i].includes("3")) {
+    newArray.push("Won't you be my neighbor?")
+  } else if (numberArray[i].includes("2")) {
+    newArray.push("Boop!")
+  } else if (numberArray[i].includes("1")) {
+    newArray.push("Beep!")
   } else {
     newArray.push(numberArray[i])
   }
   }
   return newArray;
 }
-
 
 
 $(document).ready(function () {
