ead82bdffdf0991cb98d3d1456094f59a11a0237
gbrough <gbrough@gmail.com>
2021-06-11 13:32:05 -0700

    add push method to function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
-const numberArray = [0,1,2,3];
-const newArray = []
+const numberArray = [0,1,2,3,4];
+let newArray = []
 
 function getRoboMessage(numberinput) {
   for (i = 0; i < numberArray.length; i++) {
+    if (i === 1) {
+    newArray.push(" beep!")
+  } else if (i === 2) {
+    newArray.push(" boop!")
+  } else {
     newArray.push(numberArray[i])
   }
-   return newArray; 
+  }
+  return newArray;
 }
-getRoboMessage(2);
+
 
 
 $(document).ready(function () {
