d956291ed3b31a5f858c30be3d6b84ea582725e2
gbrough <gbrough@gmail.com>
2021-06-16 21:23:20 -0700

    update array to search numberinput

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
-const numberArray = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33"];
-let newArray = []
+function clearArray() {
+  return newArray = []
+}
 
 function getRoboMessage(numberinput) {
-  for (i = 0; i < numberArray.length; i++) {
-    if (numberArray[i].includes("3")) {
+  for (i = 0; i <= numberinput; i++) {
+    if (i.toString().includes("3")) {
     newArray.push("Won't you be my neighbor?")
-    } else if (numberArray[i].includes("2")) {
+    } else if (i.toString().includes("2")) {
     newArray.push("Boop!")
-    } else if (numberArray[i].includes("1")) {
+    } else if (i.toString().includes("1")) {
     newArray.push("Beep!")
     } else {
-     newArray.push(numberArray[i])
+     newArray.push(i)
     }
   }
-  return newArray.slice(0,numberinput+1);
+  return newArray ;
 }
 
 $(document).ready(function () {
@@ -22,5 +23,5 @@ $(document).ready(function () {
     const numberinput = parseInt($("input#numberinput").val());
     const newArray = getRoboMessage(numberinput)
     $(".output").text(newArray);
-  })
-})
+    });
+});
